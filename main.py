e3fbffd5d131ff9fe77a6481d8addc1f08174aa6
Aleksandra Fedorova <asf2003@yandex.ru>
2021-11-25 20:55:09 +0300

    Now we can read an amount and type of purchase

--- main.py
+++ main.py
@@ -23,18 +23,33 @@ window = Window()
 form = Form()
 form.setupUi(window)
 window.show()
+columns= ['tr', 'pr', 'fl', 'amount', 'day', 'month', 'year']
 try:
     file = pd.read_csv('my_by.csv')
 except IOError as e:
     print(u'не удалось найти файл, так что создадим его')
-    file = pd.DataFrame({'tr' : [], 'pr' : [], 'fl' : [],
-    	'amount' : [], 'day' : [], 'month' : [], 'year' : []})
+    file = pd.DataFrame(columns= columns)
+else:
+	print('файл уже существует')
 print(file)
 
+
 def on_click():
-	print(form.lineEdit.text())
-	print(form.listWidget.item(1).isSelected())
+	text = form.lineEdit.text()
+
+	print(text)
+	new_data = [0]*7
+	for i in range(3):
+		new_data[i] = int(form.listWidget.item(i).isSelected())
+		print(form.listWidget.item(i).isSelected())
+		
+	if text.isnumeric():
+		new_data[3] = int(text)
+	else:
+		print('Не число')
+	
 	print(form.calendarWidget.selectedDate())
+	print(new_data)
 	print("You clicked ok")
 
 form.pushButton.clicked.connect(on_click)
