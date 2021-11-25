b8d92b082aa942b2fcd1c70ba26e395ae421a414
Aleksandra Fedorova <asf2003@yandex.ru>
2021-11-25 20:15:40 +0300

    now we have user's database

--- main.py
+++ main.py
@@ -13,6 +13,7 @@
 # sys.exit(app.exec_())
 
 from PyQt5 import uic
+import pandas as pd
 from PyQt5.QtWidgets import QApplication
 
 Form, Window = uic.loadUiType("interf.ui")
@@ -22,12 +23,18 @@ window = Window()
 form = Form()
 form.setupUi(window)
 window.show()
-form.listWidget.addItem("Trrrrr")
+try:
+    file = pd.read_csv('my_by.csv')
+except IOError as e:
+    print(u'не удалось найти файл, так что создадим его')
+    file = pd.DataFrame({'tr' : [], 'pr' : [], 'fl' : [],
+    	'amount' : [], 'day' : [], 'month' : [], 'year' : []})
+print(file)
 
 def on_click():
 	print(form.lineEdit.text())
-
-	print(form.listWidget.selectedItems())
+	print(form.listWidget.item(1).isSelected())
+	print(form.calendarWidget.selectedDate())
 	print("You clicked ok")
 
 form.pushButton.clicked.connect(on_click)
