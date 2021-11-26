0d2ba9ed1ab72eebe59630e5e31a733c2b807c8a
Aleksandra Fedorova <asf2003@yandex.ru>
2021-11-26 14:35:34 +0300

    new window

--- main.py
+++ main.py
@@ -24,12 +24,17 @@ form = Form()
 form.setupUi(window)
 window.show()
 
+Form_all, Window_all = uic.loadUiType("interf_show.ui")
+window_all = Window_all()
+form_all = Form_all()
+form_all.setupUi(window_all)
+window_all.hide()
 
-
-def on_click():
+#--------------------------------------------------------------------
+def on_click_enter():
 	text = form.lineEdit.text()
 	date = form.calendarWidget.selectedDate()
-	columns= ['tr', 'pr', 'fl', 'amount', 'day', 'month', 'year']
+	columns= ['other','tr', 'pr', 'fl', 'amount', 'day', 'month', 'year']
 	try:
 	    file = pd.read_csv('my_by.csv')
 	except IOError as e:
@@ -38,11 +43,11 @@ def on_click():
 	else:
 		print('файл уже существует')
 
-
-	new_data = [0]*7
-	for i in range(3):
+	new_data = [0]*8
+	for i in range(1,4):
 		new_data[i] = int(form.listWidget.item(i).isSelected())
-
+	if new_data[1:4] == [0,0,0]:
+		new_data[0] = 1
 	new_data[4] = int(form.calendarWidget.selectedDate().day())
 	new_data[5] = int(form.calendarWidget.selectedDate().month())
 	new_data[6] = int(form.calendarWidget.selectedDate().year())
@@ -57,8 +62,30 @@ def on_click():
 	print(f'file:\n{file}')
 	file.to_csv('my_by.csv', index=False)
 	print("You clicked ok")
+#---------------------------------------------------------------------
+def on_click_show():
+	window.hide()
+	window_all.show()
+	
+	
+	print("You clicked Show All")
 
-form.pushButton.clicked.connect(on_click)
+
+#-----------------------------------------------------------------------
+def on_click_back():
+	window_all.hide()
+	window.show()
+	
+	
+	print("You clicked Add Purchase")
+
+
+#-----------------------------------------------------------------------
+
+form.pushButton.clicked.connect(on_click_enter)
+form.pushButton_2.clicked.connect(on_click_show)
+form_all.pushButton_2.clicked.connect(on_click_back)
 
 
 app.exec()
+
